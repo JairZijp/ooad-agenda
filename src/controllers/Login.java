5b120ab65b0d0941d460e37844ae2e4d2b409371
jairz <jair@dutchwebdevelopment.nl>
2018-10-06 22:08:16 +0200

    singleton auth class

--- src/controllers/Login.java
+++ src/controllers/Login.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import models.Auth;
 
 public class Login {
 
@@ -26,35 +27,24 @@ public class Login {
     private PasswordField PasswordField;
 
     @FXML
-    private void NextScreen(ActionEvent event) throws IOException, SQLException, NoSuchAlgorithmException {
-        // create new User
-        User user = new User();
+    private void NextScreen(ActionEvent event) throws IOException, SQLException, NoSuchAlgorithmException {       
 
         // set username and password from textfields
         String username = UsernameField.textProperty().get(),
                 password = PasswordField.textProperty().get(),
                 hashedPassword = User.HashPassword(password);
 
-        //create connection and execute query
-        DB Connection = new DB();
-
-        //this query will only select 1 row if the data is correct
-        String sql = String.format("SELECT * FROM user WHERE `username` = '%s' AND `password` = '%s'",
-                username, hashedPassword);
-
-        ResultSet queryResult = Connection.executeResultSetQuery(sql);
-
-        //get the first row from the returned data and set User-properties
-        if (queryResult.first()) {
-
-            //set properties of current User
-            user.setUsername(queryResult.getString("username"));
+        
+        Auth auth = Auth.getInstance();
+        
+        boolean validUser = auth.validate(username, hashedPassword);
+        
+        if (validUser) {
 
             //Navigate back to old screen
             Main.GoToScreen("Home.fxml");
         }
-//        else {
-//            ErrorLabel.setText("Incorrect username/password");
-//        }
+
     }
+    
 }
