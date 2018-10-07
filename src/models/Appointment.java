9b96c51f6e8f1386f86e627f168cd5c1e3774173
JairZijp <jairzijp123@gmail.com>
2018-10-07 21:43:07 +0200

    small thinggg

--- src/models/Appointment.java
+++ src/models/Appointment.java
@@ -18,6 +18,63 @@ public abstract class Appointment {
     private Date date;
     private Time time;
     private String category;
+
+    public Appointment(String name, Date date, Time time, String category) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+        this.category = category;
+    }
+    
+    public void setAppointment() {
+        
+         //Make Connection
+        DB Connection = new DB();
+        String sql = String.format("INSERT INTO appointment (name, date, time, category)" +
+                        "VALUES ('%s', '%s', '%s', '%s')",
+                this.name, this.date, this.time, this.category);
+
+        //execute query and close connection
+        Connection.executeUpdateQuery(sql);
+        Connection.close();
+        
+    }
+    
+    /* getter and setters */
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+   
     
     
 }
